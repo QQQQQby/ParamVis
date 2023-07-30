Fix bugs and finish basic logic. Use NumPy to accelerate calculation.

param_eqs.py
@@ -1,6 +1,8 @@
 import math
 from typing import List, Tuple
 
+import numpy as np
+
 
 class ParamEq:
     def __init__(self, **kwargs):
@@ -9,7 +11,6 @@ class ParamEq:
 
     def __setattr__(self, key, value):
         super().__setattr__(key, value)
-        print(key, value)
 
     def __getattr__(self, key):
         return self.__dict__.get(key, 0)
@@ -35,15 +36,10 @@ class CircleParamEq(ParamEq):
         return ans
 
 
-class HypocycloidParamEq(ParamEq):
+class HypotrochoidParamEq(ParamEq):
     def get_points(self) -> List[Tuple[int, int]]:
-        theta = 0
-        end = 2 * math.pi
-        ans = []
-        while theta < end:
-            ans.append((
-                (self.a - self.b) * math.cos(theta) + self.b * math.cos(theta * (self.a - self.b) / self.b),
-                (self.a - self.b) * math.sin(theta) - self.b * math.sin(theta * (self.a - self.b) / self.b)
-            ))
-            theta += 0.001
-        return ans
+        end = self.R * self.k * 2 * np.pi / np.gcd(int(self.R), int(self.k * self.R))
+        thetas = np.linspace(0, end, 100000)
+        xs = self.R * ((1 - self.k) * np.cos(thetas) + self.l * self.k * np.cos((1 - self.k) / self.k * thetas))
+        ys = self.R * ((1 - self.k) * np.sin(thetas) + self.l * self.k * np.sin((1 - self.k) / self.k * thetas))
+        return [tuple(p) for p in zip(xs, ys)]