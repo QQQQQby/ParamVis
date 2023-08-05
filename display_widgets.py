7948d4c1ce210ecb0c44a64b66e68a6f1a1ad43a
QQQQQby <yx41_qby@126.com>
2023-08-05 13:50:17 +0800

    Add spinbox, draw axis and support scale and move.

--- display_widgets.py
+++ display_widgets.py
@@ -1,7 +1,7 @@
 from typing import Union
 
-from PySide2.QtCore import QPoint
-from PySide2.QtGui import QPaintEvent, QPainter, QPen, QColor, QPainterPath
+from PySide2.QtCore import QPointF
+from PySide2.QtGui import QPaintEvent, QPainter, QPen, QColor, QPainterPath, QMouseEvent, QTransform, QWheelEvent
 from PySide2.QtWidgets import QWidget
 
 
@@ -9,11 +9,22 @@ class ParamEqDisplayWidget(QWidget):
     def __init__(self, parent, *args, **kwargs):
         super().__init__(parent, *args, **kwargs)
         self.setMinimumSize(400, 400)
+        self.setMouseTracking(True)
 
         self.param_eq = None
+        self.points = None
+
+        self.transform_scale = QTransform()
+        self.transform_move = QTransform()
+        self.transform_move.translate(200, 200)
+        self.transform_move.scale(1, -1)
+        self.prev_moved = None
+
+        self.interval = 10
 
     def set_param_eq(self, param_eq_type, *args, do_repaint=True):
         self.param_eq = param_eq_type(*args)
+        self.points = self.param_eq.get_points()
         if do_repaint:
             self.repaint()
 
@@ -21,31 +32,121 @@ class ParamEqDisplayWidget(QWidget):
         if self.param_eq is None:
             raise RuntimeError('Parametric equation is None!')
         setattr(self.param_eq, key, value)
+        self.points = self.param_eq.get_points()
         if do_repaint:
             self.repaint()
+
+    def calc_real_coord(self, p: QPointF) -> QPointF:
+        return self.transform_move.map(self.transform_scale.map(p))
+
+    def wheelEvent(self, event: QWheelEvent) -> None:
+        scale_range = (0.25, 10)
+        step = 1.1
+
+        angle = event.angleDelta() / 8
+        angleY = angle.y()
+        if angleY > 0:  # Zoom in
+            self.transform_scale.scale(step, step)
+            scale = self.transform_scale.m11()
+            if scale > scale_range[1]:
+                t = scale_range[1] / scale
+                self.transform_scale.scale(t, t)
+
+        else:  # Zoom out
+            self.transform_scale.scale(1 / step, 1 / step)
+            scale = self.transform_scale.m11()
+            if scale < scale_range[0]:
+                t = scale_range[0] / scale
+                self.transform_scale.scale(t, t)
+
+        self.repaint()
+
+    def mousePressEvent(self, event: QMouseEvent) -> None:
+        self.prev_moved = QPointF(event.x(), event.y())
+        self.repaint()
+
+    def mouseMoveEvent(self, event: QMouseEvent) -> None:
+        if self.prev_moved:
+            curr_moved = QPointF(event.pos())
+            offset = curr_moved - self.prev_moved
+            self.transform_move.translate(offset.x(), -offset.y())
+            self.prev_moved = curr_moved
+
+            self.repaint()
+
+    def mouseReleaseEvent(self, event: QMouseEvent) -> None:
+        self.prev_moved = None
 
     def paintEvent(self, event: QPaintEvent) -> None:
         painter = QPainter()
         painter.begin(self)
 
-        # Draw border
-        painter.setPen(QPen(QColor(255, 0, 0), 3))
-        painter.drawLine(0, 0, self.width() - 1, 0)
-        painter.drawLine(0, 0, 0, self.height() - 1)
-        painter.drawLine(self.width() - 1, 0, self.width(), self.height())
-        painter.drawLine(0, self.height() - 1, self.width(), self.height())
+        # Draw axis
+        painter.setPen(QPen(QColor(128, 128, 128), 3))
+        center = self.calc_real_coord(QPointF(0, 0))
+        painter.drawLine(center.x(), 0, center.x(), self.height() - 1)
+        painter.drawLine(0, center.y(), self.width() - 1, center.y())
+
+        # Draw ticks
+        tick_length = 5
+
+        i = self.interval
+        while True:
+            p0 = self.calc_real_coord(QPointF(i, 0))
+            if p0.x() >= self.width():
+                break
+            p1 = QPointF(p0)
+            p1.setY(p0.y() - tick_length)
+            painter.drawLine(p0, p1)
+            i += self.interval
+
+        i = -self.interval
+        while True:
+            p0 = self.calc_real_coord(QPointF(i, 0))
+            if p0.x() < 0:
+                break
+            p1 = QPointF(p0)
+            p1.setY(p0.y() - tick_length)
+            painter.drawLine(p0, p1)
+            i -= self.interval
+
+        i = self.interval
+        while True:
+            p0 = self.calc_real_coord(QPointF(0, i))
+            if p0.y() < 0:
+                break
+            p1 = QPointF(p0)
+            p1.setX(p0.x() + tick_length)
+            painter.drawLine(p0, p1)
+            i += self.interval
+
+        i = -self.interval
+        while True:
+            p0 = self.calc_real_coord(QPointF(0, i))
+            if p0.y() >= self.height():
+                break
+            p1 = QPointF(p0)
+            p1.setX(p0.x() + tick_length)
+            painter.drawLine(p0, p1)
+            i -= self.interval
 
         # Draw Param Eq
-        if self.param_eq is not None:
-            points = self.param_eq.get_points()
-            center = QPoint(self.width() // 2, self.height() // 2)
+        painter.setPen(QPen(QColor(255, 0, 0), 3))
+
+        if self.points is not None:
             path = QPainterPath()
+            path.moveTo(self.calc_real_coord(QPointF(*self.points[0])))
+            for i in range(1, len(self.points)):
+                path.lineTo(self.calc_real_coord(QPointF(*self.points[i])))
 
-            path.moveTo(center + QPoint(*points[0]))
-            for i in range(1, len(points)):
-                path.lineTo(center + QPoint(*points[i]))
-
-            painter.setPen(QPen(QColor(255, 0, 0), 3))
             painter.drawPath(path)
+
+        # Draw border
+        border_width = 5
+        border_color = QColor(0, 0, 0)
+        painter.fillRect(0, 0, self.width(), border_width, border_color)
+        painter.fillRect(0, 0, border_width, self.height(), border_color)
+        painter.fillRect(0, self.height() - border_width, self.width(), border_width, border_color)
+        painter.fillRect(self.width() - border_width, 0, border_width, self.height(), border_color)
 
         painter.end()
--- option_widgets.py
+++ option_widgets.py
@@ -1,5 +1,5 @@
 from PySide2.QtCore import Qt
-from PySide2.QtWidgets import QWidget, QLabel, QHBoxLayout, QGridLayout
+from PySide2.QtWidgets import QWidget, QLabel, QHBoxLayout, QGridLayout, QDoubleSpinBox
 
 from custom_widgets import DoubleSlider
 from display_widgets import ParamEqDisplayWidget
@@ -27,17 +27,50 @@ class OptionWidget(QWidget):
             item_layout.addWidget(label)
 
             slider = DoubleSlider(self)
+            spinbox = QDoubleSpinBox(self)
+
             slider.setOrientation(Qt.Orientation.Horizontal)
             slider.setMinimum(arg[2])
             slider.setMaximum(arg[3])
             slider.setInterval(arg[4])
             slider.setValue(arg[5])
-            slider.doubleValueChanged.connect(self._make_slider_callback(arg[0]))
             item_layout.addWidget(slider)
+
+            spinbox.setMinimum(arg[2])
+            spinbox.setMaximum(arg[3])
+            spinbox.setSingleStep(arg[4])
+            spinbox.setValue(arg[5])
+            item_layout.addWidget(spinbox)
 
             item_layout.addStretch(1)
 
             self.main_layout.addLayout(item_layout, i // 4, i % 4)
 
-    def _make_slider_callback(self, key):
-        return lambda value: self.param_eq_display_widget.set_param_eq_args(key, value, True)
+            slider.doubleValueChanged.connect(self._make_slider_callback(arg[0], spinbox))
+            spinbox.valueChanged.connect(self._make_spinbox_callback(arg[0], slider))
+
+    def _make_slider_callback(self, key, spinbox: QDoubleSpinBox):
+        def func(value):
+            return self.on_slider_value_changed(key, value, spinbox)
+
+        return func
+
+    def _make_spinbox_callback(self, key, slider: DoubleSlider):
+        def func(value):
+            return self.on_spinbox_value_changed(key, value, slider)
+
+        return func
+
+    def on_slider_value_changed(self, key, value, spinbox: QDoubleSpinBox):
+        value = round(value, 2)
+        spinbox.blockSignals(True)
+        spinbox.setValue(value)
+        spinbox.blockSignals(False)
+        return self.param_eq_display_widget.set_param_eq_args(key, value)
+
+    def on_spinbox_value_changed(self, key, value, slider: DoubleSlider):
+        value = float(value)
+        slider.blockSignals(True)
+        slider.setValue(value)
+        slider.blockSignals(False)
+        return self.param_eq_display_widget.set_param_eq_args(key, value)
